760dd77cb13cf8ea1a939e4772d617dfe230d09e
Laercio Silva <laerciokelson@gmail.com>
2024-10-11 19:45:16 +0100

    Change the confing for making a backup copy

--- api/config/backup.php
+++ api/config/backup.php
@@ -166,7 +166,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => 'laerciokelson@gmail.com',
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
